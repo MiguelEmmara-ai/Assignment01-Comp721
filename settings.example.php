9d9bb7c575e80f1fcfc873e4892f3ed93d18f91d
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-06-05 23:38:20 +1200

    add settings.example.php

---
+++ settings.example.php
@@ -0,0 +1,15 @@
+<!--Author: Muhamad Miguel Emmara-->
+<!--Student ID: 18022146-->
+<!--Email: ryf2144@autuni.ac.nz-->
+
+<!--
+    Description of File:
+Database Connection File In PHP
+-->
+
+<?php
+$host = "localhost";
+$user = "root";
+$pswd = "YourPassword123";
+$dbnm = "YourDatabaseName";
+?>
